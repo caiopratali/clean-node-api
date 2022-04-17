899be08b7859224415d55638c461d4683c8fc186
Caio Pratali <caio@pratali.com.br>
2022-04-17 18:33:14 -0300

    test: ensure signUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signup';
+
+describe('SignUp Controller', () => {
+  test('Shold return 400 if no name if provided', () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        Confirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
